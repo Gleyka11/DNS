a7864b8ac8c53e06a5d67cfff0db7f2fc645be9a
gleyka11 <gleykagonzaga@gmail.com>
2024-07-26 17:03:37 -0300

    atividade

--- Atividade_DNS/proxy-nginx/run.sh
+++ Atividade_DNS/proxy-nginx/run.sh
@@ -10,7 +10,10 @@ echo "Criando a rede..."
 
 docker network create -d bridge gleyka_asa_br  
 
-echo "criando containers..."
+echo "Criando containers..."
 
-docker run -d --name web01 -p 8081:80 web01
-docker run -d --name web02 -p 8082:80 web02
+docker run -d --net=gleyka_asa_br --name web01 -p 8081:80 web01
+docker run -d --net=gleyka_asa_br --name web02 -p 8082:80 web02
+docker run -d --net=gleyka_asa_br --name proxy -p 8085:80 proxy
+
+
